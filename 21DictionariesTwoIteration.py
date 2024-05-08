7ee80f132a267473ae962098ac8951ed0cda1552
Dev-Aaminah <aminakanwal12345@gmail.com>
2024-05-08 19:19:55 +0500

    Two Iteration Variables in Python

---
+++ 21DictionariesTwoIteration.py
@@ -0,0 +1,6 @@
+# this program will print key value pairs
+# we have two iteration variables "aaa" and "bbb"
+
+jjj = {'chuck':1, 'fred':32, 'john':200}
+for aaa, bbb in jjj.items():
+    print(aaa, bbb)
